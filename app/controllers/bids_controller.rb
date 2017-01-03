ce5ebee50a8baede37331b9bd68187ada6bc64aa
delamorinierejh <delamorinierejh@gmail.com>
2017-01-03 12:01:43 +0000

    changing for readme

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,6 +1,6 @@
 class BidsController < ApplicationController
   before_action :set_bid, only: [:show, :update, :destroy]
-  
+
   # GET /bids
   def index
     @bids = Bid.all
@@ -15,16 +15,16 @@ class BidsController < ApplicationController
 
   # POST /bids
   def create
-  @bid = current_user.bids.new(bid_params)
+    @bid = current_user.bids.new(bid_params)
 
-  if @bid.save
-    render json: @bid, status: :created, location: @bid
-  else
-    render json: @bid.errors, status: :unprocessable_entity
+    if @bid.save
+      render json: @bid, status: :created, location: @bid
+    else
+      render json: @bid.errors, status: :unprocessable_entity
+    end
+
   end
-
-end
-
+  
   # PATCH/PUT /bids/1
   def update
     if @bid.update(bid_params)
@@ -40,13 +40,13 @@ end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_bid
-      @bid = Bid.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_bid
+    @bid = Bid.find(params[:id])
+  end
 
-    # Only allow a trusted parameter "white list" through.
-    def bid_params
-      params.require(:bid).permit(:amount, :auction_id, :user_id)
-    end
+  # Only allow a trusted parameter "white list" through.
+  def bid_params
+    params.require(:bid).permit(:amount, :auction_id, :user_id)
+  end
 end
